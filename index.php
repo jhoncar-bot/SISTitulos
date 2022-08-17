ddcc7cfeb5d4d8320c5324dacb9b92cfafbcbfc6
Jhon Castillo <gpro1pro@gmail.com>
2022-08-17 11:08:50 -0500

    cambio 2

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 	include("cabecera.php");
 ?>
 <div class="jumbotron text-center">
-  <h1>Sistemas de Cosulta de titulos</h1>
+  <h1>Sistemas de Cosulta de titulos sadsadsa</h1>
   <p>Pagina de inicio del sistema de Titulos!</p>
 </div>
 <p>
