cacf3a7fef293855de1e27626e4da3a80498930d
Jhon Castillo <gpro1pro@gmail.com>
2022-08-21 18:39:20 -0500

    conexion cambiada

--- administrador/conexion.php
+++ administrador/conexion.php
@@ -3,7 +3,7 @@
     $servername = "localhost";   #Localhost o IP
     $username = "root";          #Usuario de la dB
     $password = "";   #Contraseña de la dB
-    $database = "titulov4.1";       #Nombre de la db
+    $database = "titulov4";       #Nombre de la db
     $conn = mysqli_connect($servername, $username, $password, $database);
     if (!$conn) {
         die("Conexion no establecida: " . mysqli_connect_error());
